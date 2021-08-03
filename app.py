5ccc5ad8ae258246f9763c5de07e17233222a07b
Daryl Fernandes <36784577+DarylFernandes99@users.noreply.github.com>
2021-08-03 16:13:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
+server = app.server
 app.title = "India Population"
 
 # =============================================================================
